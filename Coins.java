61b90516d7abba10351b8ee30e8446c43a170dda
Itay <you@example.com>
2023-12-21 07:58:08 +0200

    I changed Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,10 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int cents = Integer.parseInt(args[0]);
+		int numberQuarters = cents / 25; 
+		int remainder = cents % 25; 
+		System.out.println("Use " + numberQuarters + " quarters and "  + remainder  + " cents");
+
 	}
 }
