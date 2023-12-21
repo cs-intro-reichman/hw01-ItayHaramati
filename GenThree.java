ab391a7712ab766d661b828a477fa54903afaa84
Itay <you@example.com>
2023-12-21 18:49:46 +0200

    I changed GenThree

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,15 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[0]);
+		int randomnum1 = a + (int)(Math.random() * ((b - a) + 1));
+		int randomnum2 = a + (int)(Math.random() * ((b - a) + 1));
+		int randomnum3 = a + (int)(Math.random() * ((b - a) + 1));
+		System.out.println(randomnum1);
+		System.out.println(randomnum2);
+		System.out.println(randomnum3);
+		System.out.println("The minimal generated number was " + a);
+
 	}
 }
